146e0e67d2bda1301e41f7ce2281cf9f561e9626
Ezequiel Alarcon <111069716+AlarconEzequiel285@users.noreply.github.com>
2022-08-24 22:56:36 -0300

    Add files via upload

---
+++ TP04/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String nombre = "Ezequiel Alarcon";
+    int edad = 21;
+    float salario = 157000;
+    boolean logico = true;
+
+        System.out.println("Nombre: " + nombre + "\nEdad: " + edad + "\nSalario: " + salario);
+    }
+}
